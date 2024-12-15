766e39227d4f7c6afcb8b4d22bb412cd826fbc57
Aanshija <99870125+Aanshija@users.noreply.github.com>
2024-12-15 22:42:58 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_session import Session
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Afrik'
+    app.config['SECRET_KEY'] = 'AaniPapa'
     app.config['SESSION_TYPE'] = 'filesystem'
     Session(app)
 
