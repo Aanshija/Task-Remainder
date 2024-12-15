f60a97cea62853b93d63f5151e84b9272bf0c96f
Aanshija <99870125+Aanshija@users.noreply.github.com>
2024-12-15 22:35:34 +0530

    Add files via upload

---
+++ app/firebase_connect.py
@@ -0,0 +1,27 @@
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+import json
+from dotenv import load_dotenv
+load_dotenv() 
+
+firebase_key_contents = os.getenv('FIREBASE_KEY_CONTENTS')
+if not firebase_key_contents:
+    raise ValueError("The FIREBASE_KEY_CONTENTS environment variable is not set.")
+try:
+    firebase_key_dict = json.loads(firebase_key_contents)
+except json.JSONDecodeError as e:
+    raise ValueError("The FIREBASE_KEY_CONTENTS environment variable is not a valid JSON.") from e
+
+cred = credentials.Certificate(firebase_key_dict)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def get_firestore_client():
+    """Returns the Firestore client instance."""
+    return db
+
+
+
