b92cf792c4556548bfdbdacaf7852b77b0823eb4
Aanshija <99870125+Aanshija@users.noreply.github.com>
2024-12-15 22:33:29 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY']='CHAN'
-    from .auth import auth
-    from .views import views
-    app.register_blueprint(auth)
-    app.register_blueprint(views)
-    return app
--- app/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-views = Blueprint('views', __name__)
-import os
-
-@views.route('/')
-def home():
-    return render_template('index.html')
-
